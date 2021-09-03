d7b739ea7fc7211414e7967c86577a7975cff21b
kiran <kskirankumar969@gmail.com>
2021-09-03 16:19:24 +0530

    first commit

---
+++ src/main/java/com/example/demo/Security/ApplicationUserRole.java
@@ -0,0 +1,17 @@
+package com.example.demo.Security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.example.demo.Security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
+    private final Set<ApplicationUserPermission> permissions;
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions){
+        this.permissions = permissions;
+    }
+
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    public static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"kiran"),
+            new Student(2,"kumar"),
+            new Student(3,"sai")
+    );
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId)  {
+
+        return STUDENTS.stream().filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() ->
+                    new IllegalStateException("student"+ studentId+"does not exist"));
+
+    }
+}
