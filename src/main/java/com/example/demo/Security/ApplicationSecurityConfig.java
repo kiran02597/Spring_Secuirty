083aa549196414e9bb01511c5e9536a08de9b1bd
kiran <kskirankumar969@gmail.com>
2021-09-06 17:49:59 +0530

    updated with form based and CSRF

--- src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.demo.Security;
 
+import jdk.nashorn.internal.runtime.regexp.joni.ApplyCaseFoldArg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,10 +15,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demo.Security.ApplicationUserPermission.*;
 import static com.example.demo.Security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -27,13 +32,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                //.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
-                .permitAll()
+                .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name()) //role based authentication
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                // .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses",true);
     }
 
     @Override
@@ -43,17 +56,27 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                                 .builder()
                                 .username("sai")
                                 .password(passwordEncoder.encode("password"))
-                                .roles(STUDENT.name())
+                              //  .roles(STUDENT.name())
+                                .authorities(STUDENT.getGrantedAuthorities())
                                 .build();
         UserDetails kiranUser = User
                 .builder()
                 .username("kiran")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+                //.roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+        UserDetails tonyUser = User
+                .builder()
+                .username("tony")
+                .password(passwordEncoder.encode("password1"))
+               // .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
         return  new InMemoryUserDetailsManager(
                 saiUser,
-                kiranUser
+                kiranUser,
+                tonyUser
         );
 
     }
--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -16,4 +16,12 @@ public class Student {
         this.studentId = studentId;
         this.studentName = studentName;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
 }
