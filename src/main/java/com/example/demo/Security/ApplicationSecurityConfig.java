b5927a575d63f5019470ac769d8ddc76dc0a22ba
kiran <kskirankumar969@gmail.com>
2021-09-11 20:04:17 +0530

    added and updated

--- src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.Security.ApplicationUserPermission.*;
 import static com.example.demo.Security.ApplicationUserRole.*;
@@ -46,7 +49,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 // .httpBasic();
                 .formLogin()
                 .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses",true);
+                .defaultSuccessUrl("/courses",true)
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))// CSRF diabled so GET
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login");
+
     }
 
     @Override
