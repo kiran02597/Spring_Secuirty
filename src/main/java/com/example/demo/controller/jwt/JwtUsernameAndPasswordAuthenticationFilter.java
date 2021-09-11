c5284b81c085f0b14ff22da971b988976d13b340
kiran <kskirankumar969@gmail.com>
2021-09-11 23:41:10 +0530

    started JSON web tokens

---
+++ src/main/java/com/example/demo/controller/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UsernameAndPasswordAuthenticationRequest usernameAndPasswordAuthenticationRequest =
+                                        new ObjectMapper().readValue(request.getInputStream(),
+                                                UsernameAndPasswordAuthenticationRequest.class);
+            Authentication authentication= new UsernamePasswordAuthenticationToken(
+                    usernameAndPasswordAuthenticationRequest.getUsername(),
+                    usernameAndPasswordAuthenticationRequest.getPassword()
+            );
+            Authentication authenticate = authenticationManager.authenticate(authentication);
+            return authenticate;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
